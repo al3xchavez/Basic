af83af91e5f3ca5e7685f93006641ef02d7f285c
Alex <al3xchavez@gmail.com>
2025-06-25 21:36:41 -0700

    added median function

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 print("hello_world")
+
+#median function
+def median(input):
+  input.sort()
+  length = len(input)-1              #length of string
+  middle = math.floor(length/2)      #median value
+  if length % 2 == 0:
+    umiddle = middle + 1
+    uppermiddle = input[umiddle]
+    lowermiddle = input[middle]
+    avgmiddle = (uppermiddle + lowermiddle) / 2
+    return avgmiddle
+  else:
+    return input[middle]
